ef73d4b5b251d5d203b32e86b61a18f80f8599e7
DurgaManohar-15 <durgamanohat@gamil.com>
2025-12-16 10:15:10 +0530

    Day 1: Java basics-variables and datatypes

---
+++ src/explicit.java
@@ -0,0 +1,9 @@
+public class explicit {
+    public static void main(String[] args) {
+        double d = 17.11;
+
+        int i = (int)d;
+
+        System.out.println(i);
+    }
+}
---
+++ src/miniproject.java
@@ -0,0 +1,31 @@
+class Student {
+    String name;
+    int age;
+    int marks;
+ 
+    Student(String name, int age, int marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
+
+public class miniproject {
+
+    public static void main(String[] args) {
+
+
+        Student s1 = new Student("Ravi", 20, 85);
+        Student s2 = new Student("Anita", 19, 78);
+        Student s3 = new Student("Kiran", 21, 90);
+
+        int total = s1.marks + s2.marks + s3.marks;
+        double avg = total / 3.0;
+
+        System.out.println("Name\tAge\tMarks");
+        System.out.println(s1.name + "\t" + s1.age + "\t" + s1.marks);
+        System.out.println(s2.name + "\t" + s2.age + "\t" + s2.marks);
+        System.out.println(s3.name + "\t" + s3.age + "\t" + s3.marks);
+        System.out.println("Average Marks: " + avg);
+    }
+}
